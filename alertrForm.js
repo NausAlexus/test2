89fbc6596d890ae7f2078bd06b6bb19b10f307ed
DenisBardashevich <ackapbinka@mail.ru>
2025-03-11 20:45:41 +0300

    fix

---
+++ alertrForm.js
@@ -0,0 +1,18 @@
+
+
+document.getElementById("phoneNumber").addEventListener("input", function (e) {
+    this.value = this.value.replace(/[^0-9+-]/g, ""); // Разрешает только цифры, + и -
+});
+function validateForm() {
+    let phoneNumber = document.getElementById("phoneNumber");
+    let privacyPolicy = document.getElementById("privacy-policy");
+
+    if (phoneNumber.checkValidity() && privacyPolicy.checkValidity()) {
+        alert("Registration/Login was successful");
+    } else {
+        phoneNumber.reportValidity();
+        privacyPolicy.reportValidity();
+    }
+}
+
+document.getElementById("registerButton").addEventListener("click", validateForm);
